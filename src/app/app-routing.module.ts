b2cf92e629a65fa4062c36ae9e7b0096e6d6f9ca
pedrojpineda <pedrojosepineda@hotmail.com>
2021-06-09 19:35:40 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,26 +11,6 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
-    path: 'entradas',
-    loadChildren: () => import('./entradas/entradas.module').then( m => m.EntradasPageModule)
-  },
-  {
-    path: 'principales',
-    loadChildren: () => import('./principales/principales.module').then( m => m.PrincipalesPageModule)
-  },
-  {
-    path: 'postres',
-    loadChildren: () => import('./postres/postres.module').then( m => m.PostresPageModule)
-  },
-  {
-    path: 'bebidas',
-    loadChildren: () => import('./bebidas/bebidas.module').then( m => m.BebidasPageModule)
-  },
-  {
-    path: 'cocteles',
-    loadChildren: () => import('./cocteles/cocteles.module').then( m => m.CoctelesPageModule)
-  },
 ];
 
 @NgModule({
