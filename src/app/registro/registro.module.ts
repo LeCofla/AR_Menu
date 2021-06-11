01b947daaf155776ff93892f870d66fac9a9b07f
Johann <elflacorama@gmail.com>
2021-06-11 16:34:26 -0500

    Controles adicionales para el form

--- src/app/registro/registro.module.ts
+++ src/app/registro/registro.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, NgForm} from '@angular/forms';
 
@@ -17,4 +17,34 @@ import { RegistroPage } from './registro.page';
   ],
   declarations: [RegistroPage]
 })
-export class RegistroPageModule {}
+export class RegistroPageModule implements OnInit{
+  @ViewChild('nombre', {static: true}) nombre: ElementRef;
+  @ViewChild('tarjeta', {static: true}) tarjeta: ElementRef;
+  @ViewChild('expiracion', {static: true}) expiracion: ElementRef;
+  @ViewChild('seguridad', {static: true}) seguridad: ElementRef;
+
+  userChkOut = {
+    nombre: '',
+    tarjeta: '',
+    expiracion: '',
+    seguridad: ''
+  };
+
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/contextual-lifecycle
+  ngOnInit(): void{
+  }
+
+  enviarCheckOut(): void {
+    this.userChkOut.nombre = this.nombre.nativeElement.value;
+    this.userChkOut.tarjeta = this.tarjeta.nativeElement.value;
+    this.userChkOut.expiracion = this.expiracion.nativeElement.value;
+    this.userChkOut.seguridad = this.seguridad.nativeElement.value;
+    console.log(this.userChkOut);
+    if (this.userChkOut.nombre !== '' && this.userChkOut.tarjeta !== '') {
+      document.getElementById('confirmaPago').classList.remove('d-none');
+    }
+  }
+}
+
