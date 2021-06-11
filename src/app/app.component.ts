78e68e7ca78d02972471834ecaf1b14f935798c1
pedrojpineda <pedrojosepineda@hotmail.com>
2021-06-11 13:35:41 -0500

    Se agregó ion-fab e ion chip para registro y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    console.log(this.route.snapshot.params);
+  }
 }
